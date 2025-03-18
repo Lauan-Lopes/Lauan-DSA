6a27dc406a456a7b5a6e64e2c8c37305a4c5eac5
Lauan Edward Lopes <lauanlopes99@hotmail.com>
2025-03-18 13:30:02 -0300

    Update and rename 16. LIKE.sql to SQL Server/16. LIKE.sql

--- SQL Server/16. LIKE.sql
+++ SQL Server/16. LIKE.sql
@@ -8,14 +8,14 @@ SELECT
 	*FROM DimEmployee
 WHERE FirstName + '' + LastName LIKE 'Aaron Painter'
 
--- Começa com Aaron
+-- ComeÃ§a com Aaron
 
 SELECT 
 	FirstName + '' + LastName AS 'FullName',
 	*FROM DimEmployee
 WHERE FirstName + '' + LastName LIKE 'Aaron%'
 
--- Que contém Paul em qualquer parte da string
+-- Que contÃ©m Paul em qualquer parte da string
 SELECT 
 	FirstName + '' + LastName AS 'FullName',
 	*FROM DimEmployee
@@ -27,13 +27,13 @@ SELECT
 	*FROM DimEmployee
 WHERE FirstName + '' + LastName LIKE '%Allen'
 
--- Começa com Michael
+-- ComeÃ§a com Michael
 SELECT 
 	FirstName + '' + LastName AS 'FullName',
 	*FROM DimEmployee
 WHERE FirstName + '' + LastName LIKE 'Michael%'
 
--- Começa com Nur__
+-- ComeÃ§a com Nur__
 SELECT 
 	FirstName + '' + LastName AS 'FullName',
 	*FROM DimEmployee
@@ -51,7 +51,7 @@ SELECT
 	*FROM DimEmployee
 WHERE FirstName + '' + LastName LIKE '%nev__'
 
--- Contém Gonzales com Z ou S
+-- ContÃ©m Gonzales com Z ou S
 SELECT 
 	FirstName + '' + LastName AS 'FullName',
 	*FROM DimEmployee
